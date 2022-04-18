3bbba5c4470b1fb6bcf4629ae0314c9f5fabd371
Franklin Bladimir <67939160+fbladimir@users.noreply.github.com>
2022-04-18 00:24:08 -0400

    Add files via upload
    operators

---
+++ operators_and_Equality/script.js
@@ -0,0 +1,33 @@
+//Arithmetic Operators 
+
+const a = 5; 
+const b = 10; 
+
+let result = 0; 
+
+//addition 
+result = a + b; 
+//sub 
+result = a - b; 
+//exponent 
+result = b ** a; 
+
+//modulo 
+result = a % b; 
+
+
+//increment Decrement 
+result++; 
+result--; 
+
+//Comparison operators and equality  > t/f 
+
+const a2 = 5; 
+const b2 = 10; 
+
+console.log(a > b); //gives false because a2 is not greater than b2 
+
+//strict equaltiy === 
+//strict inequality !== 
+
+
