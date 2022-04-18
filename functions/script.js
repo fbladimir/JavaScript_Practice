c6026d87f36ff0427c5cffe4a749dfd301eba278
Franklin Bladimir <67939160+fbladimir@users.noreply.github.com>
2022-04-18 00:23:18 -0400

    Add files via upload
    functions

---
+++ functions/script.js
@@ -0,0 +1,39 @@
+
+//function declaration (defining a function)
+function square(number) { 
+    return number * number; 
+}
+
+
+//a function call (calling or execution a function)
+
+const result = square(5); 
+console.log(result); 
+
+//declaring and invoking functions 
+
+//function expression 
+const name = function(params) { 
+    //statements 
+}
+
+//arrow function 
+const name2 = (params) => { 
+    //statements 
+}
+
+
+//MORE ABOUT ARROW FUNCTIONS 
+const square = (number) => number * number; 
+
+const result2 = square(2); 
+
+console.log(result2); 
+
+
+//NOTES ABOUT PARAMETERS AND ARGUMENTS 
+
+//PARAMETERS: Used when defining a func 
+//ARGUMENTS: passed when making a fun call 
+
+
